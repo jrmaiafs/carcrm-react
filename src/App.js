c09c1af74f87f862dead1615b0557a6ecc84bcc2
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-06-18 15:06:23 -0300

    initial commit for car-store react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Routes from "./Routes";
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+  },
+  props: {
+    MuiTextField: {
+      variant: "outlined",
+      fullWidth: true,
+    },
+    MuiSelect: {
+      variant: "outlined",
+      fullWidth: true,
+    },
+  },
+});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  </Provider>
+);
 
 export default App;
